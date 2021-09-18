c09235d42fdeb8b4a445b9d00d6170a536989174
Alex Derbenev <alex.derbenew@yandex.ru>
2021-09-18 18:26:30 +0300

    Добавил strict-boolean-expressions

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,20 +29,39 @@ module.exports = {
       extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
       plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
       rules: {
+        // В процессе разработки часто приходится заводить неиспользуемые переменные
         '@typescript-eslint/no-unused-vars': 0,
+        // Prettier расставляет скобочки, поэтому при базовых знаниях двоичной логики логические выражения не должны вызывать проблем
         '@typescript-eslint/no-mixed-operators': 0,
+        // Периодически приходится писать any, чтобы экономить время
         '@typescript-eslint/no-explicit-any': 0,
+        // Лишние типы это не страшно
         '@typescript-eslint/no-inferrable-types': 0,
+        // Это для нас не критично
         '@typescript-eslint/interface-name-prefix': 0,
+        // Автоматическое выведение типов помогает писать меньше кода
         '@typescript-eslint/explicit-function-return-type': 0,
+        // Автоматическое выведение типов помогает писать меньше кода
         '@typescript-eslint/explicit-module-boundary-types': 0,
+        // Иногда приходится костылять, но делать это надо с комментариями
         '@typescript-eslint/ban-ts-comment': [
           1,
           {
             'ts-ignore': 'allow-with-description',
           },
         ],
+        // Пустые интерфейсы могут быть в промежуточном состоянии кода
         '@typescript-eslint/no-empty-interface': 0,
+        // Защищает от классической ошибки с if (0)
+        '@typescript-eslint/strict-boolean-expressions': [
+          1,
+          {
+            allowNumber: false,
+            allowNullableBoolean: true,
+            allowNullableString: true,
+            allowAny: true,
+          },
+        ],
       },
     },
   ],
